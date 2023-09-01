8a79edfb039ccf23ec265bc6c0efbd498fd6ac72
arhab213 <medriadarhab@gmail.com>
2023-09-01 10:06:07 +0100

    The first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let UserDisp = document.getElementById("userGame");
+let compDisp = document.getElementById("computerGame");
+let resultDev = document.getElementById("result");
+rock.addEventListener("click", () => {
+  resultDev.innerHTML = "";
+  UserDisp.innerHTML = "";
+  compDisp.innerHTML = "";
+  let userChoice = "rock";
+  Computergame();
+  display(userChoice, computerChoice);
+  gameplay(userChoice, computerChoice);
+});
+paper.addEventListener("click", () => {
+  resultDev.innerHTML = "";
+  UserDisp.innerHTML = "";
+  compDisp.innerHTML = "";
+  let userChoice = "paper";
+  Computergame();
+  display(userChoice, computerChoice);
+  gameplay(userChoice, computerChoice);
+});
+scissors.addEventListener("click", () => {
+  resultDev.innerHTML = "";
+  UserDisp.innerHTML = "";
+  compDisp.innerHTML = "";
+  let userChoice = "scissors";
+  Computergame();
+  display(userChoice, computerChoice);
+  gameplay(userChoice, computerChoice);
+});
+
+function Computergame() {
+  let randomChoice = Math.floor(Math.random() * 3);
+  let arr = [
+    { game: "paper", index: 0 },
+    { game: "scissors", index: 1 },
+    { game: "rock", index: 2 },
+  ];
+  arr.forEach((e) => {
+    if (randomChoice == parseInt(e.index)) {
+      computerChoice = e.game;
+    }
+  });
+}
+function gameplay(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    let Tieelem = document.createElement("span");
+    Tieelem.innerHTML = "Tie!";
+    resultDev.appendChild(Tieelem);
+  }
+  if (
+    (userChoice === "paper" && computerChoice == "scissors") ||
+    (userChoice === "rock" && computerChoice == "paper") ||
+    (userChoice === "scissors" && computerChoice === "rock")
+  ) {
+    let SpanComp = document.getElementById("ComputerScore");
+    SpanComp.innerHTML = parseInt(SpanComp.innerHTML) + 1;
+  }
+  let SpanUser = document.getElementById("yourScore");
+  SpanUser.innerHTML = parseInt(SpanUser.innerHTML) + 1;
+}
+function display(userChoice, computerChoice) {
+  let gamContainer = [
+    { name: "paper", obj: "🖐" },
+    { name: "rock", obj: "✊" },
+    { name: "scissors", obj: "✂️" },
+  ];
+  gamContainer.forEach((e) => {
+    if (e.name == userChoice) {
+      let newdiv = document.createElement("div");
+      newdiv.innerHTML = e.obj;
+      UserDisp.appendChild(newdiv);
+    }
+    if (e.name == computerChoice) {
+      let newdiv = document.createElement("div");
+      newdiv.innerHTML = e.obj;
+      compDisp.appendChild(newdiv);
+    }
+  });
+}
